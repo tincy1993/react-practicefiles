7ff4fec41b60c2bf864973d265e7dad1fcf4f98d
Tincy <georgebtincy@gmail.com>
2020-06-19 09:53:38 +0530

    all files added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
+import Validation from './Validation/Validation';
+import Char from './Char/Char';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // project 1
+  // state = {
+  //   // userName: "Tincy",
+  //   userInput: ""
+  // }
+
+  // userNameChangeHandler = (event) => {
+  //   this.setState ({
+  //     userName: event.target.value
+  //   })
+  // }
+  // project 1
+  // inputChangedHandler = ( event ) => {
+  //   this.setState( { userInput: event.target.value } );
+  // }
+
+  // deleteCharHandler = ( index ) => {
+  //   const text = this.state.userInput.split('');
+  //   text.splice(index, 1);
+  //   const updatedText = text.join('');
+  //   this.setState({userInput: updatedText});
+  // }
+  
+  render() {
+    // project 2
+    // const charList = this.state.userInput.split('').map((ch, index) => {
+    //   return <Char 
+    //     character={ch} 
+    //     key={index}
+    //     clicked={() => this.deleteCharHandler(index)} />;
+    // });
+
+    return (
+      <div className="App">
+        {/* Project 1 */}
+        {/* <UserInput changed={this.userNameChangeHandler} currentName={this.state.userName} />
+        <UserOutput userName={this.state.userName} />
+        <UserOutput userName={this.state.userName} />
+        <UserOutput userName="Tyson" /> */}
+
+        {/* Project 2 
+        <input
+          type="text"
+          onChange={this.inputChangedHandler}
+          value={this.state.userInput} />
+        <p>{this.state.userInput}</p>
+        <Validation inputLength={this.state.userInput.length} />
+        {charList} */}
+      </div>
+    )
+  }
 }
 
 export default App;
